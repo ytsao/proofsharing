5573a6917f3b178a47b157c2d684132cfccc05de
ytsao <yi-nung.tsao@uni.lu>
2024-09-25 17:24:15 +0200

    update the directory

--- utils.py
+++ utils.py
@@ -848,7 +848,8 @@ def load_dataset_selected_labels_only(dataset_name, labels=None, num_elements=No
 
 
 def load_deepg_specs(idx, folder):
-    specs_file = '{}deepg/{}/{}.csv'.format(PATH_EXAMPLES, folder, idx)
+    # specs_file = '{}deepg/{}/{}.csv'.format(PATH_EXAMPLES, folder, idx)
+    specs_file = f"../../deepg/{folder}/{idx}.csv"
 
     with open(specs_file, 'r') as fin:
         lines = fin.readlines()
